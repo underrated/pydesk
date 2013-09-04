a975a8545435506ce5f09957f60c5b51cafe5b1f
underrated <yourstruly@underrated.org>
2013-09-04 11:13:25 +0300

    minor changes

--- test_flip_flop.py
+++ test_flip_flop.py
@@ -5,28 +5,31 @@ class flip_flop(Component):
 		Component.__init__(self,name,parent)
 		self.clk = state_variable(0,"clk",self)
 		self.clk.bit_size = 1
-		self.clk.first_value = 1
-		self.clk.prev_value = 1
+		# self.clk.first_value = 1
+		# self.clk.prev_value = 1
 		self.in_s = state_variable(0,"in_s",self)
 		self.in_s.bit_size = 1
 		self.out_s = state_variable(0,"out_s",self)
 		self.out_s.bit_size = 1
 
 	def on_clk(self):
-		print "time:"+str(self.sim.time)+" Clock rise : Clock changed from "+str(self.clk.prev_value)+" to "+str(self.clk.value)
 		self.out_s <= self.in_s.value
 
 	def on_clk_fall(self):
-		print "time:"+str(self.sim.time)+" Clock fall : Clock changed from "+str(self.clk.prev_value)+" to "+str(self.clk.value)
+		pass
+#		print "time:"+str(self.sim.time)+" Clock fall : Clock changed from "+str(self.clk.prev_value)+" to "+str(self.clk.value)
 
 	def on_clk_change(self):
-		print "*SAMPLE* time:"+str(self.sim.time)+" clk:"+str(self.clk.value)+" in_s:"+str(self.in_s.value)+" self.out_s:"+str(self.out_s.value)
+		pass
+#		print "*SAMPLE* time:"+str(self.sim.time)+" clk:"+str(self.clk.value)+" in_s:"+str(self.in_s.value)+" self.out_s:"+str(self.out_s.value)
 	
 	def on_out_s_change(self):
-		print "time:"+str(self.sim.time)+" Output changed from "+str(self.out_s.prev_value)+" to "+str(self.out_s.value)
+		pass
+#		print "time:"+str(self.sim.time)+" Output changed from "+str(self.out_s.prev_value)+" to "+str(self.out_s.value)
 
 	def on_in_s_change(self):
-		print "time:"+str(self.sim.time)+" Input changed from "+str(self.in_s.prev_value)+" to "+str(self.in_s.value)
+		pass
+#		print "time:"+str(self.sim.time)+" Input changed from "+str(self.in_s.prev_value)+" to "+str(self.in_s.value)
 
 	def run(self):
 		print "Setting up flip flop"
@@ -45,35 +48,27 @@ class ff_tb(Component):
 	def drive_clk(self):
 		self.clk_thread = yield sim_cmd,get_thread_self
 		self.ff.clk.value = 1
-		print "time="+str(self.sim.time)+" clk="+str(self.ff.clk.value)
 		while True:
 			#print "Old clock is "+str(self.ff.clk.value)
 			yield wait_time,5
-			self.ff.clk.value = self.ff.clk.value ^ 1
+			self.ff.clk.value = int(not self.ff.clk.value)
 			#print "New clock should be "+str(self.ff.clk.value^1)
 			#print "New actually is "+str(self.ff.clk.value^1)
 
 	def drive_signals(self):
-		print "===Step 1==="
-		print "time="+str(self.sim.time)+" clk="+str(self.ff.clk.value)
 		self.ff.in_s.value= 0
-		for i in range(3): yield sync_event, self.ff.clk.rise
-		print "===Step 2==="
-		print "time="+str(self.sim.time)+" clk="+str(self.ff.clk.value)
+		for i in range(3): yield wait_event, self.ff.clk.rise
 		self.ff.in_s.value = 1
-		for i in range(3): yield sync_event, self.ff.clk.rise
-		print "===Step 3==="
-		print "time="+str(self.sim.time)+" clk="+str(self.ff.clk.value)
+		for i in range(3): yield wait_event, self.ff.clk.rise
 		self.ff.in_s.value = 0
-		for i in range(2): yield sync_event, self.ff.clk.rise
-		print "time="+str(self.sim.time)+" Killing clock thread"
+		for i in range(2): yield wait_event, self.ff.clk.rise
 		self.kill_thread(self.clk_thread)
 	
 	def run(self):
 		print "Setting up tb"
 		# TODO : The order in which they are started matters - it shouldn't
-		self.start(self.drive_signals())
 		self.start(self.drive_clk())
+		self.start(self.drive_signals())
 
 sim = Simulator()
 top = ff_tb("top",sim)
